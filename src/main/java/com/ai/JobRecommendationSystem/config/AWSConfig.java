ce2bc52310fd0c377fd2a256e4b4ce3bf1fdf800
gangasravanthig <gangasravanthigollamudi@gmail.com>
2025-05-24 21:27:17 -0400

    Textract Bean

---
+++ src/main/java/com/ai/JobRecommendationSystem/config/AWSConfig.java
@@ -0,0 +1,32 @@
+package com.ai.JobRecommendationSystem.config;
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.textract.AmazonTextract;
+import com.amazonaws.services.textract.AmazonTextractClientBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AWSConfig {
+    @Value("${aws.access.key}")
+    String accessKey;
+    @Value("${aws.secret.key}")
+    String secretKey;
+    @Bean
+    public AmazonTextract amazonTextract() {
+        // Replace these with values from your configuration
+
+
+
+
+        BasicAWSCredentials awsCreds = new BasicAWSCredentials(accessKey, secretKey);
+
+        return AmazonTextractClientBuilder.standard()
+                .withRegion(Regions.US_EAST_1) // Change to your region
+                .withCredentials(new AWSStaticCredentialsProvider(awsCreds))
+                .build();
+    }
+}
